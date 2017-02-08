0b1c6eebe00d599f8eb7154f53c4b53b29e2fcc5
Sajal Narang <sajalnarang@gmail.com>
2017-02-08 23:55:17 +0530

    Implemented AlarmService

--- app/src/main/java/com/example/sajalnarang/smsdemo/AlarmReceiver.java
+++ app/src/main/java/com/example/sajalnarang/smsdemo/AlarmReceiver.java
@@ -1,162 +1,19 @@
 package com.example.sajalnarang.smsdemo;
 
-import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.telephony.SmsManager;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Calendar;
 
 /**
  * Created by sajalnarang on 4/12/16.
  */
 public class AlarmReceiver extends BroadcastReceiver {
     Context context;
-    private static final String SMS_SEND_ACTION = "CTS_SMS_SEND_ACTION";
-    private static final String SMS_DELIVERY_ACTION = "CTS_SMS_DELIVERY_ACTION";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         this.context = context;
-
-        Calendar calendar = Calendar.getInstance();
-        int day = calendar.get(Calendar.DAY_OF_WEEK);
-        int weekday = 0;
-        switch (day) {
-            case Calendar.MONDAY:
-                weekday = 0;
-                break;
-            case Calendar.TUESDAY:
-                weekday = 1;
-                break;
-            case Calendar.WEDNESDAY:
-                weekday = 2;
-                break;
-            case Calendar.THURSDAY:
-                weekday = 3;
-                break;
-            case Calendar.FRIDAY:
-                weekday = 4;
-                break;
-            case Calendar.SATURDAY:
-                weekday = 5;
-                break;
-            case Calendar.SUNDAY:
-                weekday = 6;
-                break;
-        }
-        GsonModels.Result weekdayResult = Data.getMenuResponse().getResults().get(weekday);
-        String message = "Breakfast: " + weekdayResult.getBreakfast() + "\n" + "Lunch: " + weekdayResult.getLunch() + "\n" + "Tiffin: " + weekdayResult.getTiffin() + "\n" + "Dinner: " + weekdayResult.getDinner();
-
-        for (String number : MainActivity.numbers) {
-            sendSMS(number, message);
-        }
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("SMS Sent")
-                        .setContentText(message)
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(message));
-// Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(context, MainActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-// Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(MainActivity.mId, mBuilder.build());
-    }
-
-    private void sendSMS(String phoneNumber, String message) {
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
-
-        PendingIntent sentPI = PendingIntent.getBroadcast(context, 0,
-                new Intent(SENT), 0);
-
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(context, 0,
-                new Intent(DELIVERED), 0);
-
-        //---when the SMS has been sent---
-        context.getApplicationContext().registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(context, "SMS sent",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(context, "Generic failure",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(context, "No service",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(context, "Null PDU",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(context, "Radio off",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(SENT));
-
-        //---when the SMS has been delivered---
-        context.getApplicationContext().registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(context, "SMS delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(context, "SMS not delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(DELIVERED));
-
-        SmsManager sms = SmsManager.getDefault();
-        ArrayList<String> parts = sms.divideMessage(message);
-        ArrayList<PendingIntent> sentIntents = new ArrayList<>();
-        ArrayList<PendingIntent> deliveryIntents = new ArrayList<>();
-
-        for (int i = 0; i < parts.size(); i++) {
-            sentIntents.add(PendingIntent.getBroadcast(context, 0, new Intent(SMS_SEND_ACTION), 0));
-            deliveryIntents.add(PendingIntent.getBroadcast(context, 0, new Intent(SMS_DELIVERY_ACTION), 0));
-        }
-
-        sms.sendMultipartTextMessage(phoneNumber, null, parts, sentIntents, deliveryIntents);
+        Intent serviceIntent = new Intent(context, AlarmService.class);
+        context.startService(serviceIntent);
     }
 }
---
+++ app/src/main/java/com/example/sajalnarang/smsdemo/AlarmService.java
@@ -0,0 +1,170 @@
+package com.example.sajalnarang.smsdemo;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class AlarmService extends IntentService {
+    private static final String SMS_SEND_ACTION = "CTS_SMS_SEND_ACTION";
+    private static final String SMS_DELIVERY_ACTION = "CTS_SMS_DELIVERY_ACTION";
+    private BroadcastReceiver deliverBroadcastReceiver;
+    private BroadcastReceiver sendBroadcastReceiver;
+
+    public AlarmService() {
+        super("Send Mess Menu");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        //---when the SMS has been sent---
+        sendBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getApplicationContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getApplicationContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getApplicationContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getApplicationContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        this.registerReceiver(sendBroadcastReceiver, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        deliverBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getApplicationContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        this.registerReceiver(deliverBroadcastReceiver, new IntentFilter(DELIVERED));
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        int weekday = 0;
+        switch (day) {
+            case Calendar.MONDAY:
+                weekday = 0;
+                break;
+            case Calendar.TUESDAY:
+                weekday = 1;
+                break;
+            case Calendar.WEDNESDAY:
+                weekday = 2;
+                break;
+            case Calendar.THURSDAY:
+                weekday = 3;
+                break;
+            case Calendar.FRIDAY:
+                weekday = 4;
+                break;
+            case Calendar.SATURDAY:
+                weekday = 5;
+                break;
+            case Calendar.SUNDAY:
+                weekday = 6;
+                break;
+        }
+        GsonModels.Result weekdayResult = Data.getMenuResponse().getResults().get(weekday);
+        String message = "Breakfast: " + weekdayResult.getBreakfast() + "\n" + "Lunch: " + weekdayResult.getLunch() + "\n" + "Tiffin: " + weekdayResult.getTiffin() + "\n" + "Dinner: " + weekdayResult.getDinner();
+
+        for (String number : MainActivity.numbers) {
+            sendSMS(number, message);
+        }
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("SMS Sent")
+                        .setContentText(message)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(message));
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(MainActivity.mId, mBuilder.build());
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+        SmsManager sms = SmsManager.getDefault();
+        ArrayList<String> parts = sms.divideMessage(message);
+        ArrayList<PendingIntent> sentIntents = new ArrayList<>();
+        ArrayList<PendingIntent> deliveryIntents = new ArrayList<>();
+
+        for (int i = 0; i < parts.size(); i++) {
+            sentIntents.add(PendingIntent.getBroadcast(this, 0, new Intent(SMS_SEND_ACTION), 0));
+            deliveryIntents.add(PendingIntent.getBroadcast(this, 0, new Intent(SMS_DELIVERY_ACTION), 0));
+        }
+
+        sms.sendMultipartTextMessage(phoneNumber, null, parts, sentIntents, deliveryIntents);
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(sendBroadcastReceiver);
+        unregisterReceiver(deliverBroadcastReceiver);
+        super.onDestroy();
+    }
+}
